146bd5505188494c73b0ec8edb4e45c4c70a6357
ROSTAND LELE <rostoinfocus@gmail.com>
2025-06-18 12:18:27 +0100

    Nombre à virgule flottante (float/double)

--- 01datatype.php
+++ 01datatype.php
@@ -25,7 +25,7 @@ echo "<pre>";
 echo(var_dump($nbr1));
 echo "</pre>";
 
-//b. Nombre à virgule flottante (float/double)
+//b. -Nombre à virgule flottante (float/double)
 
 $prix = 19.99;
 
@@ -33,10 +33,56 @@ echo "<pre>";
 echo(var_dump($prix));
 echo "</pre>";
 
+// c. Chaîne de caractères (string)
 
+$name = "Owen";
 
+echo "<pre>";
+echo(var_dump($name));
+echo "</pre>";
 
+//d. Booléen (bool| boolean)
+$disponible = true;  //ou  $disponible =false
 
+echo "<pre>";
+echo(var_dump($disponible));
+echo "</pre>";
+
+/*
+ ********   1. Types composés ****
+ * 
+*/
+
+//a. Tableau (array)
+//---  indexé (0,1,2...)
+$colors = ["green", "yellow", "red"];
+echo "<pre>";
+echo(var_dump($colors));
+echo "</pre>";
+
+$fruits =[
+    '🍎', 
+    '🍌',
+    '🍇', 
+    '🍓', 
+    '🍍', 
+    '🥭', 
+    '🍉'  
+];
+
+echo "<pre>";
+echo(var_dump($fruits));
+echo "</pre>";
+
+//Tableau associatif
+$person =[
+  "name" => "Myra",
+  "date" => 10,
+  "admin" => true
+];
+echo "<pre>";
+echo(var_dump($person));
+echo "</pre>";
   ?>
 </body>
 </html>
